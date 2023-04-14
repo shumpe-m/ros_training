bf0268254ad583576a638765075763a70e8dac5b
shunpe-hash <d1301539@gmail.com>
2023-04-14 19:18:52 +0900

    First commit

---
+++ dashing_docker/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build ./ --force-rm=true -t ros2:dashing
---
+++ dashing_docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run --gpus all --rm -it --net host --ipc host\
+    -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+    -v $HOME/ros2_ex:/root/ros2_ex \
+    ros2:dashing bash
